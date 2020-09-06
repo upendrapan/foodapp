17fc21d5d1017471d5bc0aa6133e02ee29489049
upendrapan <49208992+upendrapan@users.noreply.github.com>
2020-09-06 11:34:12 +0545

    Add files via upload

---
+++ foodapp/frontend/src/data.js
@@ -0,0 +1,32 @@
+export default {
+  products: [
+      {
+      _id: '1',
+      name: 'Butter Naan',
+      image: '/images/d1.png',
+      price: 60,
+      category: 'Naan',
+      rating: 4.5,
+      numReviews: 10
+  },
+  {
+      _id: '2',
+      name: 'Burger',
+      image:'/images/d3.jpg',
+      price: 120,
+      category: 'Burger',
+      rating: 4.5,
+      numReviews: 10
+  },
+  {
+      _id: '3',
+      name: 'Chicken Tandoori',
+      image:'/images/d2.png',
+      price: 199,
+      category: 'Chicken',
+      rating: 4,
+      numReviews: 8
+  },
+
+]
+}
